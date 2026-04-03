750e1f9c269799f772aa13766985f68128554795
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-03 15:42:22 +0530

    Time: 0 ms (100%), Space: 43.3 MB (22.4%) - LeetHub

--- LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ LeetCode/Easy/0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -17,9 +17,12 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         // preorderRecursion(root,ans);
-           preOrderStack(root,ans);
+        //    preOrderStack(root,ans);
+        // return ans;
+        
+        return preorderMorrison(root);
 
-        return ans;
+        
     }
     public void preorderRecursion(TreeNode root , List<Integer> ans){
         if(root == null)
@@ -49,5 +52,39 @@ class Solution {
             curr = stack.pop();
             curr = curr.right;
         }
+    }
+
+    public List<Integer> preorderMorrison(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        
+        //same logic as inorder just while cutting the thread we add node
+        while(curr != null){
+
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+
+            else{
+                TreeNode prev = curr.left;
+
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+
+                if(prev.right == null){
+                    prev.right = curr;
+                    ans.add(curr.val);
+                    curr = curr.left;
+                }
+                else{
+                    prev.right = null;
+
+                    curr = curr.right;
+                }
+            }
+        }
+        return ans;
     }
 }
