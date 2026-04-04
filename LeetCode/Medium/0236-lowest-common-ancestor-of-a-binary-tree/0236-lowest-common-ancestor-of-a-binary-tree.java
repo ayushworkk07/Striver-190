5a3dcc8c70e7e06f3565d52bfd7bdeccfdfa77d7
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-04 13:22:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,7 +9,7 @@
  */
 
  //1) find node to root path for both and find common point
- //2)
+ //2) call for LCA for both childs
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
@@ -19,9 +19,9 @@ class Solution {
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
 
-        if(left == null)return right;
-        else if(right==null)return left;
+        if(left == null)return right; //both are on right
+        else if(right==null)return left;    //both are on left
         else
-            return root;
+            return root;        //this is the LCA
     }
 }
