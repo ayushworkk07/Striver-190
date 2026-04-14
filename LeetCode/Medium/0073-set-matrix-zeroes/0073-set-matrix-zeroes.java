27403e345612aa0415d9bfffd22d67ac6a827183
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-14 19:55:27 +0530

    Time: 1 ms (82.97%), Space: 47.8 MB (8.35%) - LeetHub

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,37 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length , n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1; // Flag for the first column
 
-        int firstCol = 1;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++ ){
-                if(i == 0 && j == 0){
-                    firstCol = matrix[0][0];
-                    continue;
-                }
-                
-                if(matrix[i][j] == 0){
-                    matrix[0][j] = 0;
+        // Step 1: Mark the rows and columns
+        for (int i = 0; i < m; i++) {
+            // Check if first column needs to be zeroed
+            if (matrix[i][0] == 0) col0 = 0;
+            
+            // Start from second column to avoid overwriting col0 logic
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 1 ; j < n ; j++){
-                if(matrix[0][j] == 0|| matrix[i][0] == 0)
+        // Step 2: Use markers to fill zeroes (avoid first row/col for now)
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
+                }
             }
-        }
-
-        if(firstCol == 0){
-            for(int i = 0 ;i < m ; i++){
+            // Step 3: Handle the first column after the row is processed
+            if (col0 == 0) {
                 matrix[i][0] = 0;
             }
-            for(int i = 0 ;i < n ; i++){
-                matrix[0][i] = 0;
-            }
         }
-
     }
 }
