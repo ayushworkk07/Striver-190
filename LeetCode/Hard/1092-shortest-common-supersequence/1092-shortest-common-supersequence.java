6394a8f1a9bfc19c3ee1949dc4a1c712dadaaae5
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-16 19:27:39 +0530

    Time: 22 ms (87.36%), Space: 54 MB (52.6%) - LeetHub

---
+++ LeetCode/Hard/1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -0,0 +1,75 @@
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        return tabulization(str1,str2);
+    }
+
+      public int f(String text1 , String text2,int i , int j , Integer[][] dp){
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+
+        if(dp[i][j]!=null)
+            return dp[i][j];
+
+
+        // if matches add 1 and call for further LCS
+        if(text1.charAt(i) == text2.charAt(j)){
+            return dp[i][j] = 1 + f(text1,text2,i+1,j+1,dp);
+        }
+
+        //call for further combinations and take max of them 
+        return dp[i][j] = 0 + Math.max(
+            f(text1,text2,i+1,j,dp),
+            f(text1,text2,i,j+1,dp)
+        );
+    }
+
+    public String tabulization(String text1, String text2){
+        int m = text1.length() , n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = m-1; i>= 0 ;i--){
+            for(int j = n-1 ; j>= 0 ;j--){
+
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                    continue;
+                }
+
+                dp[i][j] = 0 + Math.max(
+                    dp[i+1][j],
+                    dp[i][j+1]
+                );
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0 , j = 0;
+
+        while( i < m && j < n){
+            if(text1.charAt(i) == text2.charAt(j)){
+                sb.append(text1.charAt(i));
+                i++;
+                j++;
+            }
+
+            else{
+                //we are skipping ith character , so we add it
+                if(dp[i+1][j] > dp[i][j+1]){
+                    sb.append(text1.charAt(i));
+                    i++;
+                }
+                else{
+                    sb.append(text2.charAt(j));
+                    j++;
+                }
+            }
+        }
+
+        //adding remaining character if one reaches boundary earlier
+        while(i < m) sb.append(text1.charAt(i++));
+        while(j < n) sb.append(text2.charAt(j++));
+
+        return sb.toString();
+    }
+}
