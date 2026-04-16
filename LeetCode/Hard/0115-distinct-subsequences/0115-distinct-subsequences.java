7ab2dbc41953e3acd0cfdb4d2074081ce92fa512
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-16 21:57:28 +0530

    Time: 25 ms (11.41%), Space: 57.6 MB (17.88%) - LeetHub

---
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        return f(s,t,0,0, new Integer[s.length()+1][t.length()+1]);
+        // return tabulization(s,t);
+    }
+
+    public int f(String s , String t , int i , int j , Integer[][] dp ){
+        if(j >= t.length())
+            return dp[i][j] = 1;
+
+        if(i >= s.length())
+            return dp[i][j] = 0;
+
+        if(dp[i][j] != null)
+            return dp[i][j];
+        
+        int count = 0;
+        if(s.charAt(i) == t.charAt(j)){
+            count += f(s,t,i+1,j+1,dp) + f(s,t,i+1,j,dp);
+        }
+
+        else{
+            count += f(s,t,i+1,j,dp);
+        }
+
+        return dp[i][j] = count;
+    }
+
+    public int tabulization(String s, String t){
+        int m = s.length() , n = t.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = m ; i>= 0 ;i--){
+            for(int j = n ; j>= 0 ; j--){
+                if(j >= t.length()){
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                if(i >= s.length()){
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                int count = 0;
+
+                if(s.charAt(i) == t.charAt(j))
+                    count = dp[i+1][j+1] + dp[i+1][j];
+
+                else
+                    count = dp[i+1][j+1];
+
+                dp[i][j] = count;
+            }
+        }
+
+        return dp[0][0];
+    }
+}
