f988949580db1063e42dbb1fa3781f8a6d3d825b
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-16 18:08:46 +0530

    Time: 59 ms (28.85%), Space: 63.7 MB (61.95%) - LeetHub

---
+++ LeetCode/Medium/0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String reverse = new StringBuilder(s).reverse().toString();
+        return tabulization(s,reverse);
+    }
+
+    public int f(String text1 , String text2,int i , int j , Integer[][] dp){
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+
+        if(dp[i][j]!=null)
+            return dp[i][j];
+
+
+        // if matches add 1 and call for further LCS
+        if(text1.charAt(i) == text2.charAt(j)){
+            return dp[i][j] = 1 + f(text1,text2,i+1,j+1,dp);
+        }
+
+        //call for further combinations and take max of them 
+        return dp[i][j] = 0 + Math.max(
+            f(text1,text2,i+1,j,dp),
+            f(text1,text2,i,j+1,dp)
+        );
+    }
+
+    public int tabulization(String text1, String text2){
+        int m = text1.length() , n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = m-1; i>= 0 ;i--){
+            for(int j = n-1 ; j>= 0 ;j--){
+
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                    continue;
+                }
+
+                dp[i][j] = 0 + Math.max(
+                    dp[i+1][j],
+                    dp[i][j+1]
+                );
+            }
+        }
+
+        return dp[0][0];
+    }
+}
