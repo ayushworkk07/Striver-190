90e62ccfee4bdb72b8081e5609c26f449f9425a9
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-17 12:33:31 +0530

    Time: 19 ms (39.75%), Space: 70.5 MB (50.15%) - LeetHub

---
+++ LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,74 @@
+class Solution {
+    /* at each node we try all options 
+    1) buy or skip (if bought == 0)
+    2) sell or skip (if bought == 1)
+
+    at each sell we subtract transaction fees
+    every node returns the max profit which can be made from itself to n-1 
+    */
+   int transactionFees;
+    public int maxProfit(int[] prices , int fees) {
+        transactionFees = fees;
+        // return f(prices , 0 , 0 , new Integer[prices.length+1][2]);
+
+        return tabulization(prices);
+    }
+
+    public int f(int[] prices , int index , int bought, Integer[][]dp){
+        if(index >= prices.length){
+            return dp[index][bought] = 0;
+        }
+
+        int profit = 0;
+        if(bought == 1){
+            int sell = prices[index] + f(prices, index+1,0,dp) - transactionFees;    //skipped 1 index cause of cooldown
+            int skip = f(prices,index+1,bought,dp);
+
+            profit = Math.max(sell,skip);
+        }
+
+        else{
+
+            int buy = -prices[index] + f(prices,index+1,1,dp);
+            int skip = f(prices,index+1,0,dp);
+
+            profit = Math.max(buy,skip);
+        }
+
+        return dp[index][bought] = profit;
+    }
+
+    public int tabulization(int[] prices){
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+
+        for(int index = n ; index>=0 ;index--){
+            for(int bought = 0 ; bought <= 1 ; bought++){
+                if(index >= prices.length){
+                     dp[index][bought] = 0;
+                     continue;
+                }
+
+                int profit = 0;
+                if(bought == 1){
+                    int sell = prices[index] + dp[index+1][0] - transactionFees;    //skipped 1 index cause of cooldown
+                    int skip = dp[index+1][bought];
+
+                    profit = Math.max(sell,skip);
+                }
+
+                else{
+
+                    int buy = -prices[index] + dp[index+1][1];
+                    int skip = dp[index+1][0];
+
+                    profit = Math.max(buy,skip);
+                }
+
+                 dp[index][bought] = profit;
+            }
+        }
+
+        return dp[0][0];
+    }
+}
