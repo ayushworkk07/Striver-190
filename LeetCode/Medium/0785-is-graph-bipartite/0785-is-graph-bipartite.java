1a1d59e79d79853465b9ba862e088da7502664b4
ayushworkk07 <ayush.workk07@gmail.com>
2026-04-20 10:54:52 +0530

    Time: 1 ms (63.45%), Space: 47.2 MB (41.95%) - LeetHub

---
+++ LeetCode/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,40 @@
+class Solution {
+    /* use 2 colours to paint the graph such that 2 adjacent nodes doesnt have the same
+    colour in BFS
+    */
+    public boolean isBipartite(int[][] graph) { 
+        int v = graph.length;
+
+        int[] visited = new int[v];
+
+        for(int i = 0 ; i < v ; i++){
+            if(visited[i] == 0 && bfs(graph,i,visited) == false)
+                return false;
+        }
+
+        return true;
+    }
+
+    public boolean bfs(int[][] graph,int i ,int[] visited){
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        q.add(i);
+        visited[i] = 1;
+
+        while(q.size() != 0){
+            int node = q.poll();
+
+            for(int nbr : graph[node]){
+                if(visited[nbr] == 0){
+                    visited[nbr] = visited[node] == 1 ? 2 : 1;
+                    q.add(nbr);
+                }
+
+                else if(visited[nbr] == visited[node])
+                    return false;
+            }   
+        }
+
+        return true;
+    }
+}
